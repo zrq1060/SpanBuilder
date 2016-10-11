e8568164f9250af16d90a5d35bbc9c4fcf8029c4
zhangrq <zrq1060@163.com>
2016-10-11 16:09:41 +0800

    添加图片

--- app/src/main/java/com/zrq/spanbuilderdemo/MainActivity.java
+++ app/src/main/java/com/zrq/spanbuilderdemo/MainActivity.java
@@ -44,14 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 .append(new TextSetting(" %\n", 16, Color.BLACK))
                 .append(new TextSetting("这是", 12, ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)))
                 .append(new TextSetting("特殊的", 25, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)))
-                .append(new TextSetting("内容\n", 18, ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)))
+                .append(new TextSetting("内容\n\n\n", 18, ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)))
                 // 用法 2之单一样式
-                .append(SpanBuilder.getBackgroundColorSpan("↑↑上面这个是用法1（内容、大小、颜色）\n↓↓↓↓↓↓下面这个是用法2之单一样式↓↓↓↓↓↓\n", Color.RED))
+                .append(SpanBuilder.getBackgroundColorSpan("↑↑上面这个是用法1（内容、大小、颜色）\n↓↓↓↓↓↓下面这个是用法2之单一样式↓↓↓↓↓↓\n\n" +
+                        "\n" +
+                        "\n", Color.RED))
+                .append(SpanBuilder.getBackgroundColorSpan("这是背景颜色\n", Color.RED))
                 .append(SpanBuilder.getUnderLineSpan("这是下划线\n"))
                 .append(SpanBuilder.getDeleteLineSpan("这是删除线\n"))
-                .append("这是")// 拼接没有任何样式的字符串
                 .append(SpanBuilder.getUnderLabelSpan("下标", 12))
-                .append("这是")
                 .append(SpanBuilder.getUpLabelSpan("上标\n", 14))
                 .append(SpanBuilder.getClickSpan("此内容可点击\n", textView, new ClickableSpan() {
                     @Override
@@ -69,18 +70,16 @@ public class MainActivity extends AppCompatActivity {
                 .append(SpanBuilder.getQuoteSpan("设置此段前有垂直的蓝色引用线\n", Color.BLUE))
                 .append(SpanBuilder.getTextAppearanceSpan("设置此字体自定义style样式\n", this, R.style.MyTextStyle))
                 .append(SpanBuilder.getTextColorSpan("设置字体的颜色为红色\n", Color.RED))
-                .append("后面这是一张图片")
                 .append(SpanBuilder.getImageSpan("此内容无效，会被图片给替换", drawable))
-                .append("前面这是一张图片\n");
+                .append("\n\n\n\n");
 
         // 用法 2之混合样式（高级混合拼接）
 
         // 2.1 一次生成合体
         spanBuilder.append(SpanBuilder.getBackgroundColorSpan("\n↓↓↓↓↓↓下面这个是用法2之混合样式↓↓↓↓↓↓\n", Color.RED));
         spanBuilder.append(SpanBuilder.getBackgroundColorSpan("\n1.一次生成混合体\n", Color.GREEN));
-        SpannableString span = SpanBuilder.getSpan("一次生成混合体，设置了红色字体、删除线、斜体加粗\n",
-                new ForegroundColorSpan(Color.RED), new StrikethroughSpan(), new StyleSpan(Typeface.BOLD_ITALIC));
-        spanBuilder.append(span);
+        spanBuilder.append(SpanBuilder.getSpan("一次生成混合体，设置了红色字体、删除线、斜体加粗\n",
+                new ForegroundColorSpan(Color.RED), new StrikethroughSpan(), new StyleSpan(Typeface.BOLD_ITALIC)));
 
 
         // 2.2 在原来span的基础上添加新的样式
